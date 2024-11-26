1861fabb127439ac9052bc0f270a49102a9d8aea
Brandon Horton <brandonkeiji@google.com>
2024-11-26 00:17:29 +0000

    fix: skip unauthenticated integration test case

--- integration_tests/test_notebook_output_download.py
+++ integration_tests/test_notebook_output_download.py
@@ -1,5 +1,6 @@
 import unittest
 
+import pytest
 from requests import HTTPError
 
 from kagglehub import notebook_output_download
@@ -15,6 +16,7 @@ class TestModelDownload(unittest.TestCase):
             expected_files = ["submission.csv"]
             assert_files(self, actual_path, expected_files)
 
+    @pytest.mark.skip(reason="TODO(b/380948485): support anonymous access to `/api/v1/kernels`")
     def test_download_public_notebook_output_as_unauthenticated_succeeds(self) -> None:
         with create_test_cache():
             with unauthenticated():
@@ -44,8 +46,12 @@ class TestModelDownload(unittest.TestCase):
     def test_download_large_notebook_output_warns(self) -> None:
         handle = "integrationtester/titanic-tutorial-many-output-files"
         with create_test_cache():
+            msg = f"Too many files in {handle} (capped at 25). Unable to download files."
             # If the model has > 25 files, we warn the user that it's not supported yet
-            with self.assertWarns(Warning, msg=f"Too many files in {handle}. Unable to download files."):
+            with self.assertWarns(
+                Warning,
+                msg=msg,
+            ):
                 notebook_output_download(handle)
 
     def test_download_private_notebook_output_with_incorrect_file_path_fails(self) -> None:
